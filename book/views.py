d3e206c4471a3235c208498f3f575bcadf9caf68
KirillMelanich <kirillmelanich@gmail.com>
2023-08-29 17:05:46 +0300

    Create views for  book app

--- book/views.py
+++ book/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from book.models import Book
+from book.serializers import (
+    BookSerializer,
+    BookListSerializer,
+    BookDetailSerializer,
+)
+
+
+class BookPagination(PageNumberPagination):
+    page_size = 10
+    max_page_size = 100
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    pagination_class = BookPagination
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        elif self.action == "retrieve":
+            return BookDetailSerializer
+
+        return BookSerializer
