7caedb082162d0029628cc5d38c43e2394f3e44d
KirillMelanich <kirillmelanich@gmail.com>
2023-08-29 16:56:21 +0300

    Create models for book app

--- book/models.py
+++ book/models.py
@@ -1,3 +1,24 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class Cover(models.IntegerChoices):
+        HARD = 1
+        SOFT = 2
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.PositiveSmallIntegerField(choices=Cover.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(limit_value=0)],
+    )
+
+    def __str__(self):
+        return f'"{self.title}" by {self.author}'
+
+
