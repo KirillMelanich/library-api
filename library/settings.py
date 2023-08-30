bcf79e80014e5e2b3bbe2655b3387ada5c12e221
KirillMelanich <kirillmelanich@gmail.com>
2023-08-30 17:57:47 +0300

    Create borrowins app

--- library/settings.py
+++ library/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "user",
     "book",
+    "borrowings",
 ]
 
 MIDDLEWARE = [
@@ -107,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "Europe/Kiev"
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
