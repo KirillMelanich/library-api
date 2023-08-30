d3a568363f44802f9dc32eaa3a18c00d192991c2
KirillMelanich <kirillmelanich@gmail.com>
2023-08-30 18:13:30 +0300

    Make sure all non-admins can see only their borrowings

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,20 +1,47 @@
+from django.db import models, transaction
 from rest_framework import viewsets, mixins
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from book.models import Book
 from borrowing.models import Borrowing
 from borrowing.serializers import (
     BorrowingSerializer,
     BorrowingDetailSerializer,
+    BorrowingCreateSerializer,
 )
 
 
+class BorrowingPagination(PageNumberPagination):
+    page_size = 10
+    max_page_size = 100
+
+
 class BorrowingViewSet(
-    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
 ):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    pagination_class = BorrowingPagination
 
     def get_serializer_class(self):
         if self.action == "retrieve":
             return BorrowingDetailSerializer
+        elif self.action == "create":
+            return BorrowingCreateSerializer
 
         return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        book_id = int(self.request.data.get("book"))
+        with transaction.atomic():
+            Book.objects.filter(id=book_id).update(
+                inventory=models.F("inventory") - 1
+            )
+            serializer.save(user=self.request.user)
