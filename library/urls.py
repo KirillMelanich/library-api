457f7da6225005e491bca43e47d4f19807354759
KirillMelanich <kirillmelanich@gmail.com>
2023-08-31 14:40:20 +0300

    Changed library urls

--- library/urls.py
+++ library/urls.py
@@ -16,9 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("user.urls", namespace="user")),
     path("books/", include("book.urls", namespace="book")),
+    path("borrowings/", include("borrowing.urls", namespace="borrowing")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
+
